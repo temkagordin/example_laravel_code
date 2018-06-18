ee43506aea81ffd349189d48e671304bdbac6c43
Artem <temka.gordin@gmail.com>
2018-06-18 11:56:04 +0300

    VueJS added

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example component</div>
+
+                    <div class="panel-body">
+                       This is example component
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component add')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
